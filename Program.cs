87e89f5ca0606299a1c2ccbe4d0fa9f0fe332f7e
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-05 09:53:31 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace numeroNegativo_DecisaoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("--- Numero negativo ---\n");
+            
+            Console.Write("Digite um numero: ");
+
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+            
+            if (numeroDigitado <= 0)
+            {
+                Console.WriteLine($"{numeroDigitado} está no formato negativo. ");
+            }
+            else
+            {
+                Console.WriteLine($"O numero {numeroDigitado} está correto.");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # numeroNegativo-DecisaoSimples
-Leia um número inteiro e exiba a mensagem "Você digitou um número negativo." caso o número seja menor que zero.  Ex.:  Digite um número: -12 Você digitou um número negativo. ou  Digite um número: 12 (nada a exibir)
+Leia um número inteiro e exiba a mensagem "Você digitou um número negativo."caso o número seja menor que zero.  
+
+Ex.:  Digite um número: -12 Você digitou um número negativo. 
+
+ou  Digite um número: 12 (nada a exibir)
